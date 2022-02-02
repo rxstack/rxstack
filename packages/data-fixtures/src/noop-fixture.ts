7bf25521ead99c4cc8c9371efa75fbe12ac342af
Nikolay Georgiev <symfonist@gmail.com>
2022-02-02 15:28:22 +0200

    upgraded data-fixtures

--- packages/data-fixtures/src/noop-fixture.ts
+++ packages/data-fixtures/src/noop-fixture.ts
@@ -3,7 +3,9 @@ import {Injectable} from 'injection-js';
 
 @Injectable()
 export class NoopFixture extends AbstractFixture {
-  async load(): Promise<void> { }
+  async load(): Promise<void> {
+    // do nothing
+  }
 
   getName(): string {
     return 'noop-fixture';
