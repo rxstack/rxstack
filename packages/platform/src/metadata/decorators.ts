29bf9b02a612f1872c94bd186095e7ef9c143f4b
Nikolay Georgiev <symfonist@gmail.com>
2022-02-02 17:45:31 +0200

    upgraded platform

--- packages/platform/src/metadata/decorators.ts
+++ packages/platform/src/metadata/decorators.ts
@@ -2,7 +2,7 @@ import {PLATFORM_OPERATION_KEY} from '../interfaces';
 import {OperationMetadata} from './operation.metadata';
 
 export function Operation<T extends OperationMetadata>(metadata: T): ClassDecorator {
-  return function (target: Function): void {
+  return function (target: any): void {
     metadata.extra = metadata.extra || {};
     Reflect.defineMetadata(PLATFORM_OPERATION_KEY, metadata, target);
   };
